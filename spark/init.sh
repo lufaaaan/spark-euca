9d3676d1b8afd8060c43d654cd843223812bf576
lufaaaan <125168633@qq.com>
2016-04-19 22:10:41 +0800

    Update init.sh

--- spark/init.sh
+++ spark/init.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-pushd /root
+pushd /root > /dev/null
 
 if [ -d "spark" ]; then
   echo "Spark seems to be installed. Exiting."
   return
 fi
 
-#echo "DEBUG:: SPARK_VERSION=$SPARK_VERSION"
-#echo "DEBUG:: HADOOP_MAJOR_VERSION=$HADOOP_MAJOR_VERSION"
 # Github tag:
 if [[ "$SPARK_VERSION" == *\|* ]]
 then
   mkdir spark
-  pushd spark
+  pushd spark > /dev/null
   git init
   repo=`python -c "print '$SPARK_VERSION'.split('|')[0]"` 
   git_hash=`python -c "print '$SPARK_VERSION'.split('|')[1]"`
@@ -22,75 +20,128 @@ then
   git checkout $git_hash
   sbt/sbt clean assembly
   sbt/sbt publish-local
-  popd
+  popd > /dev/null
 
 # Pre-packaged spark version:
 else 
   case "$SPARK_VERSION" in
     0.7.3)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.7.3-prebuilt-hadoop1.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.7.3-prebuilt-hadoop1.tgz
       else
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.7.3-prebuilt-cdh4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.7.3-prebuilt-cdh4.tgz
       fi
       ;;    
     0.8.0)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.8.0-incubating-bin-hadoop1.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.0-incubating-bin-hadoop1.tgz
       else
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.8.0-incubating-bin-cdh4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.0-incubating-bin-cdh4.tgz
       fi
       ;;    
     0.8.1)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.8.1-incubating-bin-hadoop1.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.1-incubating-bin-hadoop1.tgz
       else
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.8.1-incubating-bin-cdh4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.8.1-incubating-bin-cdh4.tgz
       fi
       ;;    
     0.9.0)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.9.0-incubating-bin-hadoop1.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.0-incubating-bin-hadoop1.tgz
       else
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.9.0-incubating-bin-cdh4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.0-incubating-bin-cdh4.tgz
       fi
       ;;
     0.9.1)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.9.1-bin-hadoop1.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.1-bin-hadoop1.tgz
       else
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-0.9.1-bin-cdh4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.1-bin-cdh4.tgz
+      fi
+      ;;
+    0.9.2)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.2-bin-hadoop1.tgz
+      else
+        wget http://s3.amazonaws.com/spark-related-packages/spark-0.9.2-bin-cdh4.tgz
       fi
       ;;
     1.0.0)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        #wget http://www.apache.org/dist/spark/spark-1.0.0/spark-1.0.0-bin-hadoop1.tgz
-	wget http://php.cs.ucsb.edu/spark-related-packages/spark-1.0.0-bin-hadoop1.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.0-bin-hadoop1.tgz
       else
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-1.0.0-bin-cdh4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.0-bin-cdh4.tgz
       fi
       ;;
     1.0.1)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-1.0.1-bin-hadoop1.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.1-bin-hadoop1.tgz
       else
-        wget http://php.cs.ucsb.edu/spark-related-packages/spark-1.0.1-bin-cdh4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.1-bin-cdh4.tgz
+      fi
+      ;;
+    1.0.2)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.2-bin-hadoop1.tgz
+      else
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.0.2-bin-cdh4.tgz
+      fi
+      ;;
+    1.1.0)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.0-bin-hadoop1.tgz
+      elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.0-bin-cdh4.tgz
+      else
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.0-bin-hadoop2.4.tgz
+      fi
+      ;;
+    1.1.1)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.1-bin-hadoop1.tgz
+      elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.1-bin-cdh4.tgz
+      else
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.1.1-bin-hadoop2.4.tgz
+      fi
+      ;;
+    1.2.0)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.2.0-bin-hadoop1.tgz
+      elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.2.0-bin-cdh4.tgz
+      else
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.2.0-bin-hadoop2.4.tgz
+      fi
+      ;;
+    1.2.1)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.2.1-bin-hadoop1.tgz
+      elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.2.1-bin-cdh4.tgz
+      else
+        wget http://s3.amazonaws.com/spark-related-packages/spark-1.2.1-bin-hadoop2.4.tgz
       fi
       ;;
     *)
-      echo "ERROR: Unknown Spark version"
-      return
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop1.tgz
+      elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
+        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-cdh4.tgz
+      else
+        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop2.4.tgz
+      fi
+      if [ $? != 0 ]; then
+        echo "ERROR: Unknown Spark version"
+        return -1
+      fi
   esac
 
-  #Make sure there is a spark copy
-  #wget http://www.apache.org/dist/spark/spark-1.0.0/spark-1.0.0-bin-hadoop1.tgz
   echo "Unpacking Spark"
-  tar xvzf spark-*.tgz > /tmp/spark-euca_spark.log
+  tar xvzf spark-*.tgz > /tmp/spark-ec2_spark.log
   rm spark-*.tgz
-  mv `ls -d spark-* | grep -v euca` spark
-  ls -al
-  #Makes sure the name is changed
-  #mv spark-1.0.0-bin-hadoop1 spark #If the above works this should be redundant and fail
+  mv `ls -d spark-* | grep -v ec2` spark
 fi
 
-popd
+popd > /dev/null
