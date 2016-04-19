6d77f16cfcbf714e48b8ba08d90f058adcc1e547
lufaaaan <125168633@qq.com>
2016-04-19 22:18:21 +0800

    Update init.sh

--- persistent-hdfs/init.sh
+++ persistent-hdfs/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd /root
+pushd /root > /dev/null
 
 if [ -d "persistent-hdfs" ]; then
   echo "Persistent HDFS seems to be installed. Exiting."
@@ -9,20 +9,31 @@ fi
 
 case "$HADOOP_MAJOR_VERSION" in
   1)
-    wget http://php.cs.ucsb.edu/spark-related-packages/hadoop-1.0.4.tar.gz
-    #wget https://archive.apache.org/dist/hadoop/core/hadoop-1.0.4/hadoop-1.0.4.tar.gz
+    wget http://s3.amazonaws.com/spark-related-packages/hadoop-1.0.4.tar.gz
     echo "Unpacking Hadoop"
-    tar xvzf hadoop-1.0.4.tar.gz > /tmp/spark-euca_hadoop.log
+    tar xvzf hadoop-1.0.4.tar.gz > /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
     mv hadoop-1.0.4/ persistent-hdfs/
+    cp /root/hadoop-native/* /root/persistent-hdfs/lib/native/
     ;;
   2)
-   wget http://php.cs.ucsb.edu/spark-related-packages/hadoop-2.0.0-cdh4.2.0.tar.gz
-   #wget --output-document="hadoop-2.0.0-cdh4.2.0.tar.gz" http://archive.cloudera.com/cdh4/cdh/4/bigtop-jsvc-1.0.10-cdh4.2.0.tar.gz
+    wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.0.0-cdh4.2.0.tar.gz
     echo "Unpacking Hadoop"
-    tar xvzf hadoop-*.tar.gz > /tmp/spark-euca_hadoop.log
+    tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
     mv hadoop-2.0.0-cdh4.2.0/ persistent-hdfs/
+
+    # Have single conf dir
+    rm -rf /root/persistent-hdfs/etc/hadoop/
+    ln -s /root/persistent-hdfs/conf /root/persistent-hdfs/etc/hadoop
+    cp /root/hadoop-native/* /root/persistent-hdfs/lib/native/
+    ;;
+  yarn)
+    wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.4.0.tar.gz
+    echo "Unpacking Hadoop"
+    tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
+    rm hadoop-*.tar.gz
+    mv hadoop-2.4.0/ persistent-hdfs/
 
     # Have single conf dir
     rm -rf /root/persistent-hdfs/etc/hadoop/
@@ -31,8 +42,8 @@ case "$HADOOP_MAJOR_VERSION" in
 
   *)
      echo "ERROR: Unknown Hadoop version"
-     return -1
+     return 1
 esac
-cp /root/hadoop-native/* /root/persistent-hdfs/lib/native/
-/root/spark-euca/copy-dir /root/persistent-hdfs
-popd
+/root/spark-ec2/copy-dir /root/persistent-hdfs
+
+popd > /dev/null
