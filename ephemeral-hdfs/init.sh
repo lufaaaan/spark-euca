630d825e5cb00f721663b5fd1ddd7b039f9c6e61
lufaaaan <125168633@qq.com>
2016-04-19 22:17:11 +0800

    Update init.sh

--- ephemeral-hdfs/init.sh
+++ ephemeral-hdfs/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pushd /root
+pushd /root > /dev/null
 
 if [ -d "ephemeral-hdfs" ]; then
   echo "Ephemeral HDFS seems to be installed. Exiting."
@@ -9,21 +9,32 @@ fi
 
 case "$HADOOP_MAJOR_VERSION" in
   1)
-    wget http://php.cs.ucsb.edu/spark-related-packages/hadoop-1.0.4.tar.gz
-    #wget https://archive.apache.org/dist/hadoop/core/hadoop-1.0.4/hadoop-1.0.4.tar.gz
+    wget http://s3.amazonaws.com/spark-related-packages/hadoop-1.0.4.tar.gz
     echo "Unpacking Hadoop"
-    tar xvzf hadoop-1.0.4.tar.gz > /tmp/spark-euca_hadoop.log
+    tar xvzf hadoop-1.0.4.tar.gz > /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
     mv hadoop-1.0.4/ ephemeral-hdfs/
     sed -i 's/-jvm server/-server/g' /root/ephemeral-hdfs/bin/hadoop
+    cp /root/hadoop-native/* /root/ephemeral-hdfs/lib/native/
     ;;
   2) 
-    wget http://php.cs.ucsb.edu/spark-related-packages/hadoop-2.0.0-cdh4.2.0.tar.gz
-    #wget --output-document="hadoop-2.0.0-cdh4.2.0.tar.gz" http://archive.cloudera.com/cdh4/cdh/4/bigtop-jsvc-1.0.10-cdh4.2.0.tar.gz
+    wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.0.0-cdh4.2.0.tar.gz  
     echo "Unpacking Hadoop"
-    tar xvzf hadoop-*.tar.gz > /tmp/spark-euca_hadoop.log
+    tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
     mv hadoop-2.0.0-cdh4.2.0/ ephemeral-hdfs/
+
+    # Have single conf dir
+    rm -rf /root/ephemeral-hdfs/etc/hadoop/
+    ln -s /root/ephemeral-hdfs/conf /root/ephemeral-hdfs/etc/hadoop
+    cp /root/hadoop-native/* /root/ephemeral-hdfs/lib/native/
+    ;;
+  yarn)
+    wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.4.0.tar.gz
+    echo "Unpacking Hadoop"
+    tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
+    rm hadoop-*.tar.gz
+    mv hadoop-2.4.0/ ephemeral-hdfs/
 
     # Have single conf dir
     rm -rf /root/ephemeral-hdfs/etc/hadoop/
@@ -32,8 +43,8 @@ case "$HADOOP_MAJOR_VERSION" in
 
   *)
      echo "ERROR: Unknown Hadoop version"
-     return -1
+     return 1
 esac
-cp /root/hadoop-native/* ephemeral-hdfs/lib/native/
-/root/spark-euca/copy-dir /root/ephemeral-hdfs
-popd
+/root/spark-ec2/copy-dir /root/ephemeral-hdfs
+
+popd > /dev/null
