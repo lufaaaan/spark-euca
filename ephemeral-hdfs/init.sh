c53a9c24476a8ea3d27ce8658a0b50a97c3e5f4a
lufaaaan <125168633@qq.com>
2016-04-20 11:26:06 +0800

    Update init.sh

--- ephemeral-hdfs/init.sh
+++ ephemeral-hdfs/init.sh
@@ -32,9 +32,9 @@ case "$HADOOP_MAJOR_VERSION" in
   yarn)
     wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.4.0.tar.gz
     echo "Unpacking Hadoop"
-    tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
+    tar xvzf hadoop-*.tar.gz > /tmp/spark-euca_hadoop.log
     rm hadoop-*.tar.gz
-    mv hadoop-2.4.0/ ephemeral-hdfs/
+    mv hadoop-2.0.0-cdh4.2.0/ ephemeral-hdfs/
 
     # Have single conf dir
     rm -rf /root/ephemeral-hdfs/etc/hadoop/
@@ -43,8 +43,9 @@ case "$HADOOP_MAJOR_VERSION" in
 
   *)
      echo "ERROR: Unknown Hadoop version"
-     return 1
+     return -1
 esac
-/root/spark-ec2/copy-dir /root/ephemeral-hdfs
+cp /root/hadoop-native/* ephemeral-hdfs/lib/native/
+/root/spark-euca/copy-dir /root/ephemeral-hdfs
+popd
 
-popd > /dev/null
