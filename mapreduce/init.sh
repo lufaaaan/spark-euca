3035c52aa530d0935c1444aefc6e986722c22ece
lufaaaan <125168633@qq.com>
2016-04-20 11:29:13 +0800

    Update init.sh

--- mapreduce/init.sh
+++ mapreduce/init.sh
@@ -19,5 +19,5 @@ case "$HADOOP_MAJOR_VERSION" in
      echo "ERROR: Unknown Hadoop version"
      return -1
 esac
-/root/spark-ec2/copy-dir /root/mapreduce
-popd > /dev/null
+/root/spark-euca/copy-dir /root/mapreduce
+popd
