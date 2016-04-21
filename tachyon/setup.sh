6faf04df4b46ecedcf6e2dddbb62073b95368e67
lufaaaan <125168633@qq.com>
2016-04-21 20:54:07 +0800

    Update setup.sh

--- tachyon/setup.sh
+++ tachyon/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/root/spark-euca/copy-dir /root/spark-euca/tachyon
+/root/spark-euca/copy-dir /root/spark-euca/tachyon/tachyon-0.5.0
 
-/root/spark-euca/tachyon/bin/tachyon format
+/root/spark-euca/tachyon/tachyon-0.5.0/bin/tachyon format
 
 sleep 1
 
-/root/spark-euca/tachyon/bin/tachyon-start.sh all Mount
+/root/spark-euca/tachyon/tachyon-0.5.0/bin/tachyon-start.sh all Mount
