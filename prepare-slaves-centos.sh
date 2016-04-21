206750c97820b701a67ba01b6967868843467217
lufaaaan <125168633@qq.com>
2016-04-21 17:36:47 +0800

    Update prepare-slaves-centos.sh

--- prepare-slaves-centos.sh
+++ prepare-slaves-centos.sh
@@ -5,8 +5,8 @@
 setenforce 0; #yum --assumeyes update
 yum --assumeyes install wget
 #yum -y install git
-yum --assumeyes install java-1.7.0-openjdk
-mv /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.99.x86_64/jre /usr/lib/jvm/java-1.7.0/
+#yum --assumeyes install java-1.7.0-openjdk
+#mv /usr/lib/jvm/java-1.7.0-openjdk-1.7.0.99.x86_64/jre /usr/lib/jvm/java-1.7.0/
 wget http://downloads.typesafe.com/scala/2.11.1/scala-2.11.1.tgz
 tar xvf scala-2.11.1.tgz
 mv scala-2.11.1 scala
